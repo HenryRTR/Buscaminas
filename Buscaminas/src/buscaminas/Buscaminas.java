8521eafc38ffe25b7f4496f54cb324685b77c779
HenryR <HenryR@HenryRichard>
2019-04-01 16:14:05 +0200

    Llamada a los Metodos para Iniciar el Juego

--- Buscaminas/src/buscaminas/Buscaminas.java
+++ Buscaminas/src/buscaminas/Buscaminas.java
@@ -15,7 +15,11 @@ public class Buscaminas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Juego juego = new Juego();
+        
+        juego.configurarJuego();
+        juego.iniciarJuego();
+        juego.Jugar();
     }
     
 }
