ecea1ec60c48c2bb2741924a96108dee3316aebb
HenryR <HenryR@HenryRichard>
2019-04-01 16:15:06 +0200

    Llamada a los Metodos para Iniciar el Juego

--- Buscaminas/src/buscaminas/Tablero.java
+++ Buscaminas/src/buscaminas/Tablero.java
@@ -17,16 +17,22 @@ public class Tablero {
 
     /**
      * Inicializamos la Matriz y las Casillas
-     * @param nFilas Numero de Filas que tendra el Tablero
-     * @param nColumnas Numero de Columnas que tendra el Tablero 
+     * @param numFilas Numero de Filas que tendra el Tablero
+     * @param numColumnas Numero de Columnas que tendra el Tablero 
      */
-    public Tablero(int nFilas, int nColumnas) {
-        this.numFilas = nFilas;
-        this.numColumnas = nColumnas;
+    public Tablero(int numFilas, int numColumnas) {
+        this.numFilas = numFilas;
+        this.numColumnas = numColumnas;
         this.numMinas = 0;
-        this.tabla = new Casilla[nFilas][nColumnas];
-        for (int i = 0; i < nFilas; i++) {
-            for (int j = 0; j < nColumnas; j++) {
+        this.tabla = new Casilla[numFilas][numColumnas];
+    }
+
+    /**
+     * Inicializamos la Tabla
+     */
+    public void inicializarTabla(){
+        for(int i = 0; i < this.tabla.length; i++){
+            for(int j = 0; j < this.tabla[0].length; j++){
                 tabla[i][j] = new Casilla();
             }
         }
@@ -37,16 +43,16 @@ public class Tablero {
      * @param numMinas que Tendra el Tablero
      */
     public void insertarMinas(int numMinas){
-        int fila, columna, contadorMinas = 0;
         //CREAMOS UNA MATRIZ EN LA QUE GUARDAMOS LA POSICÓN DE LAS MINAS
         int posicionMinas[][] = new int[numMinas][2];
+        int contadorMinas = 0;
         
         for (int i = 0; i < numMinas; i++) {
             //GENERAMOS LA POSICIÓN ALEATORIA DE LA MINA
-            fila = (int) (Math.random() * getNumFilas());
-            columna = (int) (Math.random() * getNumColumnas());
+            int fila = (int) (Math.random() * getNumFilas());
+            int columna = (int) (Math.random() * getNumColumnas());
             
-            if(tabla[fila][columna].isMina()){
+            if(!tabla[fila][columna].isMina()){
                 tabla[fila][columna].setMina(true);
                 //GUARDAMOS LA POSICIÓN DE LA BOMBA
                 posicionMinas[contadorMinas][0] = fila;
@@ -61,12 +67,12 @@ public class Tablero {
     
     public void imprimirPrueba(){
         for (int i = 0; i < tabla.length; i++) {
-            String fila = " ";
+            String fila = "";
             for (int j = 0; j < tabla[i].length; j++) {
                 if(tabla[i][j].isMina()){
-                    fila += "M ";
+                    fila += "M  ";
                 }else{
-                    fila = Integer.toString(this.tabla[i][j].getNumero())+" ";
+                    fila = Integer.toString(this.tabla[i][j].getNumero())+"  ";
                 }
             }
             System.out.println(fila);
@@ -79,7 +85,7 @@ public class Tablero {
      * @param columnas de la matriz
      * @return Casilla de la Tabla
      */
-    public Casilla geCasilla(int filas, int columnas){
+    public Casilla getCasilla(int filas, int columnas){
         return this.tabla[filas][columnas];
     }
     
